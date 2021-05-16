74370b1e7db5f60696a50f3697d8a0f1730ee123
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2021-05-16 21:22:59 +0100

    register Home component and use it

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <h1>Hey there</h1>
+  <Home />
 </template>
 
 <script>
+import Home from './components/Home.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Home
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+#app {}
 </style>
