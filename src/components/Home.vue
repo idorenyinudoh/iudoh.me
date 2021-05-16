8d14c2f52f6d9fb65594d1c33359af2f02a4b5f5
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2021-05-16 21:19:36 +0100

    create component for home page

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Hi.</h1>
+</template>
+
+<script>
+    export default {
+        name: 'Home'
+    }
+</script>
+
+<style scoped>
+    h1 {
+        margin-top: calc(20px + 1vw);
+        margin-left: calc(15px + 1vw);
+        font-family: sans-serif;
+    }
+</style>
