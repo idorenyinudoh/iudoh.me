b08360069ae6f622b1468ef51b156100fc83e5e3
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2021-05-17 21:08:55 +0100

    easy with the indentations

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,15 +3,15 @@ h1 Hi.
 </template>
 
 <script>
-    export default {
-        name: 'Home'
-    }
+export default {
+    name: 'Home'
+}
 </script>
 
 <style scoped>
-    h1 {
-        margin-top: calc(20px + 1vw);
-        margin-left: calc(15px + 1vw);
-        font-family: sans-serif;
-    }
+h1 {
+    margin-top: calc(20px + 1vw);
+    margin-left: calc(15px + 1vw);
+    font-family: sans-serif;
+}
 </style>
