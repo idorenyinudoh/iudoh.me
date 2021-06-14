ebc9dab84124fe0984e73351b03348df82b16fcc
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2021-06-14 08:17:31 +0100

    add little intro to home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,10 @@ header
     p Hey there,
     h1 Idorenyin Udoh, here
     p I'll be graduating with a BSc in Computer Science by next month, I work out, I often play Candy Crush and CoDM, I like to think I can rap, and most importantly, I write. Code and technical articles.
+#lil-intro
+    p I mostly write JavaScript but I'm also into HTML and CSS.
+    p Ocassionally, I work with Node.js, Express, Pug, MongoDB, and nodemon.
+    p And - I use Git all the time.
 </template>
 
 <script>
@@ -27,5 +31,13 @@ h1 {
     margin: 0;
     font-weight: 500;
     font-size: 48px;
+}
+#lil-intro {
+    border-top: 2px solid;
+    border-bottom: 2px solid;
+    padding: calc(20px + 1vw);
+}
+#lil-intro p {
+    margin: calc(10px + 1vw);
 }
 </style>
