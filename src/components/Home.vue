afc8422e16ca37c98320856b330eacbfcb966367
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2021-06-20 23:05:16 +0100

    add em dashes where necessary

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,11 +2,11 @@
 header
     p Hey there,
     h1 Idorenyin Udoh, here
-    p I'll be graduating with a BSc in Computer Science by next month, I work out, I often play Candy Crush and CoDM, I like to think I can rap, and most importantly, I write. Code and technical articles.
+    p I'll be graduating with a BSc in Computer Science by next month, I work out, I often play Candy Crush and CoDM, I like to think I can rap, and most importantly, I write. Code — and technical articles.
 #lil-intro
     p I mostly write JavaScript but I'm also into HTML and CSS.
     p Ocassionally, I work with Node.js, Express, Pug, MongoDB, and nodemon.
-    p And - I use Git all the time.
+    p And — I use Git all the time.
 section#works
     h2 stuff i've worked on
     article
