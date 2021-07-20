03ed35c19ec5788f279c966647b85f3519d4c839
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2021-07-20 09:29:29 +0100

    erase component import and registration and replace component with router-view in template

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <template>
-  <Home />
+  <router-view/>
 </template>
 
 <script>
-import Home from './components/Home.vue';
-
 export default {
-  name: 'App',
-  components: {
-    Home
-  }
+  name: 'App'
 }
 </script>
 
