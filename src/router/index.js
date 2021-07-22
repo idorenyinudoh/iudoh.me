881342a10447b86c0ecc6515cb1c45098c5d2744
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2021-07-22 15:29:45 +0100

    create route for undiluted music

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import UndilutedMusic from '../views/UndilutedMusic.vue'
 
 const routes = [
   {
@@ -7,6 +8,11 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/undiluted-music',
+    name: 'Undiluted Music',
+    component: UndilutedMusic
+  }
 //   {
     // path: '/about',
     // name: 'About',
