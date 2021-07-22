1cb5ba4ef8bcb15dc24166d4412478421c0b1418
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2021-07-22 15:27:07 +0100

    create component for each work in the works section

---
+++ src/components/Work.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+h1 {{ name }}
+p {{ summary }}
+</template>
+
+<script>
+export default {
+    name: 'Work',
+    props: {
+        name: String,
+        summary: String
+    }
+}
+</script>
+
+<style>
+
+</style>
