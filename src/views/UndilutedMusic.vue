a54bd6352248bc5f5c58c134dd1f1e1054eea0c9
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2021-07-22 15:29:10 +0100

    create view for undiluted music

---
+++ src/views/UndilutedMusic.vue
@@ -0,0 +1,28 @@
+<template>
+  <Work :="props"></Work>
+</template>
+
+<script>
+import Work from "@/components/Work.vue"
+
+const props = {
+    name: 'undiluted music',
+    summary: 'a website that gives users access to stream and download songs.'
+}
+
+export default {
+    name: 'UndilutedMusic',
+    data() {
+        return {
+            props
+        }
+    },
+    components: {
+        Work
+    }
+}
+</script>
+
+<style>
+
+</style>
