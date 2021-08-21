48767fe9660132c5b735c52abe83caec390fcf37
idorenyinudoh <idorenyinudoh10@outlook.com>
2021-08-21 14:17:02 +0100

    add path to frontend references in vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import UndilutedMusic from '../views/UndilutedMusic.vue'
+import FrontendReferences from '../views/FrontendReferences.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/undiluted-music',
     name: 'Undiluted Music',
     component: UndilutedMusic
+  },
+  {
+    path: '/frontend-references',
+    name: 'Frontend References',
+    component: FrontendReferences
   }
 //   {
     // path: '/about',
