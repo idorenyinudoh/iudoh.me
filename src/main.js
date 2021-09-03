24f5def6c0d4624beb1a12c47a0d6cbdd7cebd8d
idorenyinudoh <idorenyinudoh10@outlook.com>
2021-09-03 20:56:50 +0100

    implement google analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGTag from 'vue-gtag'
 
-createApp(App).use(router).mount('body')
+createApp(App)
+.use(router)
+.use(VueGTag, {
+    config: {
+        id: 'G-7HTEDCCE8L'
+    }
+})
+.mount('body')
