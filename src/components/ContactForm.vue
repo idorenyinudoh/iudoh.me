8f827ec4b07226cd6af997d30fc4aa7de07e62c7
idorenyinudoh <idorenyinudoh10@outlook.com>
2021-09-03 20:17:53 +0100

    the process.env thing works now

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -33,15 +33,15 @@ export default {
         },
         submitForm(e) {
             emailjs.send(
-                'service_a23ks2u',
-                'template_hflruyo',
+                process.env.VUE_APP_SERVICE_ID,
+                process.env.VUE_APP_TEMPLATE_ID,
                 {
                     name: this.name,
                     email: this.email,
                     subject: this.subject,
                     message: this.message
                 },
-                'user_KKGeOCryoJdt5V45QFpqk'
+                process.env.VUE_APP_USER_ID
             )
             .then(() => {
                 e.target.classList.add('hide')
