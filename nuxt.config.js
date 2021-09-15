768a356d884269f3d881e262725f34fe3e35a65e
idorenyinudoh <idorenyinudoh10@outlook.com>
2021-09-15 21:01:56 +0100

    set global title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'iudoh.me',
+    title: 'Idorenyin Udoh',
     htmlAttrs: {
       lang: 'en'
     },
