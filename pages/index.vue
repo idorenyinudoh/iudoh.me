0f1b602cdc02cde6acdcbaedc943fb562e19882b
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2022-01-29 16:39:05 +0100

    update employment status!!

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@ div
     p I replicate UI designs to pixel-perfect webpages with HTML, CSS, and JavaScript. I'm proficient with Vue and Nuxt, I always work with Git, and my preferred template engine is Pug.
     p I often write concise technical articles when I learn new technologies as I want to give back to the community
       small  and make a few bucks.
-    p I'm currently interning as a Software Engineer at 
+    p I'm currently working as a Software Engineer at 
       a(href='https://acumen.digital/' target='_blank') Acumen Digital
       | .
   section#works
