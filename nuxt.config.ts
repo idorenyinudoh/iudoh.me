7223e5d417108db2e4f3f70bdf4f6e64e5a15f43
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-10-07 17:18:27 +0100

    feat: install and configure google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  modules: ['nuxt-gtag']
 })
