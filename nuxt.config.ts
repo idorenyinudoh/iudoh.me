6c5533a124964f696bd138ccd2f668b8be857ca8
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2024-06-06 14:52:26 +0100

    build: install and configure nuxt color mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-gtag', '@nuxtjs/tailwindcss']
+  modules: ['nuxt-gtag', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  colorMode: {
+    classSuffix: ''
+  }
 })
