c95ee9446c117bfe5a13990f054a78c2567c560b
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2024-06-08 21:14:23 +0100

    feat: include moon icon in color mode toggle in nav

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -17,7 +17,8 @@ const changeColorMode = () => {
     </div>
     <button class="border border-solid border-[#EFEFEF] dark:border-[#434343] rounded-[32px] px-2.5 py-2 grid grid-cols-2 bg-[#F9F9F9] dark:bg-[#505050] hover:brightness-95 dark:hover:brightness-75 transition-all duration-200 ease-linear" @click="changeColorMode">
       <div :class="['w-9 h-9 rounded-full flex justify-center items-center transition-all duration-200 ease-linear', colorMode.value === 'light' ? 'bg-white': 'bg-[#1A1A1A] translate-x-full']">
-        <img src="~/assets/icons/sun.svg" alt="sun">
+        <img v-if="colorMode.value === 'light'" src="~/assets/icons/sun.svg" alt="sun">
+        <img v-else src="~/assets/icons/moon.svg" alt="moon">
       </div>
     </button>
   </nav>
