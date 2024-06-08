fc859f2421da6640a4dc186eebabe09222987cb2
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2024-06-08 16:39:55 +0100

    refactor: delete layouts and pages directories and stick to app.vue version

--- app.vue
+++ app.vue
@@ -13,10 +13,20 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
   ]
 })
+useSeoMeta({
+  title: 'Idorenyin Udoh | frontend engineer & technical writer',
+  ogTitle: 'Idorenyin Udoh | frontend engineer & technical writer',
+  twitterTitle: 'Idorenyin Udoh | frontend engineer & technical writer',
+  description: 'Idorenyin Udoh - a frontend engineer and technical writer based in Lagos, Nigeria.',
+  ogDescription: 'Idorenyin Udoh - a frontend engineer and technical writer based in Lagos, Nigeria.',
+  twitterDescription: 'Idorenyin Udoh - a frontend engineer and technical writer based in Lagos, Nigeria.',
+  ogImage: 'https://res.cloudinary.com/idorenyinudoh/image/upload/portfolio/home-hero.png',
+  twitterImage: 'https://res.cloudinary.com/idorenyinudoh/image/upload/portfolio/home-hero.png',
+  twitterCard: 'summary_large_image',
+  ogUrl: `https://iudoh.me`,
+})
 </script>
 
 <template>
-  <NuxtLayout>
-    <NuxtPage :page-key="route => route.fullPath" />
-  </NuxtLayout>
+  <h1 class="text-[80px] leading-[110%] font-medium text-[#1A1A1A] tracking-tighter">Text goes here. Text goes here. Text goes here. Text goes here. Text goes here.</h1>
 </template>
